a5f5546338a18580feb55654a782c3444fcdfeae
Julianmenav <julianmenav2@gmail.com>
2023-06-15 18:35:17 +0200

    feat: create admin panels

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Picture;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class AdminController extends Controller
@@ -10,7 +12,14 @@ class AdminController extends Controller
 
     public function view(Request $request) {
 
-        return Inertia::render('Admin/AdminPanel');
+        if( isset($request->user_id)){
+            $picturesPaginator = Picture::where('user_id', $request->user_id)->paginate(10);
+            return Inertia::render('Admin/AdminPanelPictures', ['picturesPaginator' => $picturesPaginator]);
+        }
+
+
+
+        return Inertia::render('Admin/AdminPanelUsers', ['usersPaginator' => DB::table('users')->paginate(10)]);
  
     }
 }
---
+++ resources/js/Pages/Admin/AdminPanelUsers.jsx
@@ -0,0 +1,106 @@
+import GlobalLayout from "@/Layouts/GlobalLayout";
+import { Head, Link } from "@inertiajs/react";
+import React from "react";
+
+export default function AdminPanelUsers({ auth, errors, usersPaginator }) {
+    console.log(usersPaginator);
+
+
+    const parsePaginate = (string) => {
+        if(string.includes('&laquo;')) return <>&laquo;</>
+        if(string.includes('&raquo;')) return <>&raquo;</>
+        return string
+    }
+
+    return (
+        <GlobalLayout auth={auth} errors={errors}>
+            <Head title="Admin Panel" />
+            <section className="max-w-4xl m-auto mt-10">
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Picture
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    ID
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Email
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Sign Date
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Delete
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {usersPaginator.data.map((user, id) => {
+                                return (
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={id}>
+                                        <td className="px-3 py-2 text-center">
+                                            <img
+                                                src={user.profile_pic}
+                                                className="w-8 h-8"
+                                            />
+                                        </td>
+                                        <td className="px-3 py-2 text-center">
+                                            <Link
+                                                href={route("admin.view", {
+                                                    user_id: user.id,
+                                                })}
+                                            >
+                                                {user.id}
+                                            </Link>
+                                        </td>
+                                        <td className="px-3 py-2 text-center max-w-lg truncate">{user.name}</td>
+                                        <td className="px-3 py-2 text-center max-w-lg truncate">{user.email}</td>
+                                        <td className="px-3 py-2 text-center">
+                                            {user.created_at.split(" ")[0]}
+                                        </td>
+                                        <td className="px-3 py-2 flex justify-center">
+                                            <button className="text-gray-100 bg-gray-500 p-1 opacity-70 text-sm font-bold rounded-md shadow-md hover:bg-red-500 hover:opacity-100 active:translate-y-0.5">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="currentColor"
+                                                    className="text-gray-100 w-6 h-6"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
+                                                    <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <nav className="flex items-center justify-between pt-4 " aria-label="Table navigation">
+                    <ul className="inline-flex items-center bg-zinc-100 rounded-md overflow-hidden">
+                        {
+                            usersPaginator.links.map((link) => {
+                                return (
+                                    <li>
+                                        <a href={link.url} className={`p-3 border-x border-gray-300 ${link.active && 'text-blue-400 bg-gray-200'}`}>
+                                            {parsePaginate(link.label)}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }        
+                    </ul>
+                </nav>
+            </section>
+        </GlobalLayout>
+            // <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+            // <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+);
+}

ca773314fd43c6ede7b9604f66a8e55bcf322aba
Julianmenav <julianmenav2@gmail.com>
2023-06-17 17:38:27 +0200

    chore: create language files

---
+++ lang/en/messages.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Interface Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'title' => 'Create unique images with',
+    'title_ai' => 'AI',
+    'subtitle' => 'Unleash your creativity with the power of artificial intelligence.',
+    'subtitle_2' => 'Create amazing images and share them.',
+    'button_create' => 'Start creating',
+    'button_discover' => 'Discover more',
+    'pass' => 'Password',
+    'session' => 'Keep me signed in',
+    'register' => 'Register',
+    'name' => 'Name',
+    'email' => 'Email',
+    'again' => 'Once again, please :)',
+    'already' => 'Already have an account?',
+    'create_account' => 'Create an account',
+    'disc_title' => 'Images created by other users',
+    'date' => 'Upload date',
+    'like' => 'Likes',
+    'asc' => 'ascend.',
+    'desc' => 'descend.',
+    'see_more' => 'See more',
+    'create_title' => 'Write a detailed description',
+    'create_placeholder' => 'e.g., An oil painting of a basketball player jumping',
+    'generate_button' => 'Generate Image',
+    'loading' => 'Your image is being generated',
+    'loading_2' => 'It\'s almost ready',
+    'ready' => 'Ready! Your image has been created and added to your works!',
+    'button_galery' => 'View in your gallery',
+    'button_create_2' => 'Keep creating',
+    'works' => 'Works',
+    'created' => 'Images created',
+    'stored' => 'Images saved',
+    'comments' => 'Comments',
+    'add_comment' => 'Add a comment...',
+    'comment_button' => 'Comment',
+    'edit_button' => 'Edit',
+    'edit_button_2' => 'Editing',
+    'delete_comment' => 'Delete',
+    'picture_advice' => 'Do you want to delete this image?',
+    'cancel' => 'Cancel',
+    'delete' => 'Delete',
+    'edit_profile' => 'Edit Profile',
+    'logout' => 'Log out',
+    'admin' => 'Admin Panel',
+    'upload_button' => 'Upload photo',
+    'saved_changes' => 'Changes saved',
+    'save' => 'Save changes',
+    'dangerzone' => 'Danger Zone!',
+    'danger_advice' => 'If you delete your account, it and all the images created in it will be deleted forever, think twice!',
+    'delete_account' => 'Delete account',
+    'account_advice' => 'Are you sure you want to delete this account permanently?',
+    'enter_psw' => 'Enter your password',
+    'delete_account_2' => 'I\'m crazy',
+];
---
+++ lang/es/messages.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Interface Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'title' => 'Crea imágenes únicas con',
+    'title_ai' => 'IA',
+    'subtitle' => 'Desata tu creatividad con la potencia de la inteligencia artificial.',
+    'subtitle_2' => 'Crea imágenes asombrosas y compártelas.',
+    'button_create' => 'Comienza a crear',
+    'button_discover' => 'Descubre más',
+    'pass' => 'Contraseña',
+    'session' => 'Mantener la sesión iniciada',
+    'register' => 'Regístrate',
+    'name' => 'Nombre',
+    'email' => 'Email',
+    'again' => 'Otra vez por favor :)',
+    'already' => 'Ya tienes una cuenta?',
+    'create_account' => 'Crear cuenta',
+    'disc_title' => 'Imágenes creadas por otros usuarios',
+    'date' => 'Fecha de subida',
+    'like' => 'Likes',
+    'asc' => 'ascend.',
+    'desc' => 'descend.',
+    'see_more' => 'Ver más',
+    'create_title' => 'Escribe una descripción destallada',
+    'create_placeholder' => 'p. ej: Una pinturia al óleo de un jugador de baloncesto saltando',
+    'generate_button' => 'Generar Imagen',
+    'loading' => 'Tu imagen está siendo generada',
+    'loading_2' => 'Ya está casi lista',
+    'ready' => 'Listo! Tu imagen ha sido creada y añadida a tus obras!',
+    'button_galery' => 'Ver en tu galería',
+    'button_create_2' => 'Sigue creando',
+    'works' => 'Obras',
+    'created' => 'Imágenes creadas',
+    'stored' => 'Imágenes guardadas',
+    'comments' => 'Comentarios',
+    'add_comment' => 'Añade un comentario...',
+    'comment_button' => 'Comentar',
+    'edit_button' => 'Editar',
+    'edit_button_2' => 'Editando',
+    'delete_comment' => 'Borrar',
+    'picture_advice' => 'Deseas eliminar esta imagen?',
+    'cancel' => 'Cancelar',
+    'delete' => 'Eliminar',
+    'edit_profile' => 'Editar Perfil',
+    'logout' => 'Cerrar sesión',
+    'admin' => 'Panel admin',
+    'upload_button' => 'Subir foto',
+    'saved_changes' => 'Cambios guardados',
+    'save' => 'Guardar cambios',
+    'dangerzone' => 'Zona peligrosa !',
+    'danger_advice' => 'Si borras tu cuenta, ésta y todas las imágenes creadas en la misma serán borradas para siempre, piénsalo 2 veces!',
+    'delete_account' => 'Eliminar cuenta',
+    'account_advice' => 'Estás seguro de que deseas eliminar esta cuenta para siempre?',
+    'enter_psw' => 'Introduce tu contraseña',
+    'delete_account_2' => 'Estoy loco',
+
+];
